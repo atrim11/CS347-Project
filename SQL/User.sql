5567e98c99dbec7d2c46e392fefcba234948f760
Aidan Trimmer <aidan@trimmerfamily.com>
2023-02-28 13:07:49 -0500

    Create User.sql

---
+++ SQL/User.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Feb 28, 2023 at 07:05 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `Project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `User`
+--
+
+CREATE TABLE `User` (
+  `User_ID` int(11) NOT NULL,
+  `Email` text DEFAULT NULL,
+  `Phone#` text DEFAULT NULL,
+  `Username` text DEFAULT NULL,
+  `F_Name` text DEFAULT NULL,
+  `L_Name` text DEFAULT NULL,
+  `Gender` enum('Male','Female','Other') DEFAULT 'Other',
+  `Height` int(11) DEFAULT NULL,
+  `Weight` int(11) DEFAULT NULL,
+  `Date_Joined` date DEFAULT NULL,
+  `DOB` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `User`
+--
+ALTER TABLE `User`
+  ADD PRIMARY KEY (`User_ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
