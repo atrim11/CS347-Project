8a3291a65eda70c27f70fe8628132396a8a4d5e8
Aidan Trimmer <aidan@trimmerfamily.com>
2023-03-02 13:09:05 -0500

    Update cs347_project.sql
    adding the rest of the data for the 3 other tables

--- SQL/cs347_project.sql
+++ SQL/cs347_project.sql
@@ -2,8 +2,8 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 28, 2023 at 08:21 PM
+-- Host: localhost
+-- Generation Time: Mar 02, 2023 at 07:08 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `cs347_project`
+-- Database: `Project`
 --
 
 -- --------------------------------------------------------
@@ -35,6 +35,22 @@ CREATE TABLE `comments` (
   `content` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`comment_id`, `user_id`, `post_id`, `date_time`, `content`) VALUES
+(1, 3, 1, '2023-03-20 07:19:19', 'Nice!'),
+(2, 1, 7, '2023-03-02 19:04:44', 'Good Work!'),
+(3, 2, 5, '2023-03-25 19:49:49', 'That\'s cool'),
+(4, 4, 9, '2023-03-27 05:42:10', 'Good job'),
+(5, 6, 5, '2023-03-02 19:04:44', 'go you!'),
+(6, 5, 4, '2023-03-02 19:04:44', 'looks tough'),
+(7, 5, 5, '2023-03-28 18:43:43', 'wow'),
+(8, 7, 7, '2023-03-04 19:49:49', 'Dang!'),
+(9, 8, 7, '2023-03-21 10:26:27', 'Fun!'),
+(10, 3, 8, '2023-03-17 14:15:31', 'thats crazy');
+
 -- --------------------------------------------------------
 
 --
@@ -48,6 +64,22 @@ CREATE TABLE `likes` (
   `date_time` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
+--
+-- Dumping data for table `likes`
+--
+
+INSERT INTO `likes` (`like_id`, `user_id`, `post_id`, `date_time`) VALUES
+(1, 3, 1, '2023-03-23 09:07:38'),
+(2, 8, 4, '2023-03-12 07:38:38'),
+(3, 1, 4, '2023-03-20 05:28:38'),
+(4, 5, 4, '2023-03-02 19:01:38'),
+(5, 6, 10, '2023-03-02 13:41:38'),
+(6, 4, 5, '2023-03-02 05:50:17'),
+(7, 10, 8, '2023-03-02 12:29:28'),
+(8, 1, 8, '2023-03-28 05:14:14'),
+(9, 3, 3, '2023-03-11 13:45:16'),
+(10, 8, 8, '2023-03-09 13:56:11');
+
 -- --------------------------------------------------------
 
 --
@@ -60,6 +92,22 @@ CREATE TABLE `log_posts` (
   `workout` text DEFAULT NULL,
   `date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `log_posts`
+--
+
+INSERT INTO `log_posts` (`post_id`, `user_id`, `workout`, `date`) VALUES
+(1, 2, 'Run 800 meters\r\n20 single-arm dumbbell snatches, alternating\r\n20 double dumbbell squat cleans\r\n100 sit-ups\r\n20 double dumbbell squat cleans\r\n20 single-arm dumbbell snatches, alternating\r\nRun 800 meters', '2023-03-02 18:55:00'),
+(2, 4, '5 rounds of:\r\n5 minutes of rowing\r\n5 minutes of rest', '2023-03-23 12:56:53'),
+(3, 7, '10 rounds for time of:\r\n5 pull-ups\r\n5 push-ups\r\n\r\n5 rounds for time of:\r\n10 GHD sit-ups\r\n10 back extensions\r\n\r\n2 rounds for time of:\r\n25 wall-ball shots\r\n25 box jumps\r\n\r\n', '2023-03-14 12:56:53'),
+(4, 9, '3 rounds for time of:\r\n20 Turkish get-ups\r\nRun 400 meters', '2023-03-05 12:56:53'),
+(5, 9, 'Complete as many rounds as possible in 20 minutes of:\r\n200-meter farmers carry\r\n100-meter walking lunge\r\n50-meter handstand walk', '2023-03-23 08:56:53'),
+(6, 2, 'Complete as much as possible in 20 minutes of:\r\n10 pull-ups\r\n20 push-ups\r\n30 squats\r\n15 pull-ups\r\n30 push-ups\r\n45 squats\r\n20 pull-ups\r\n40 push-ups\r\n60 squats 25 pull-ups\r\n50 push-ups\r\n75 squats\r\n30 pull-ups\r\n60 push-ups\r\n90 squats', '2023-03-31 17:23:53'),
+(7, 1, 'Complete as many rounds and repetitions as possible in 14 minutes of:\r\n\r\n60-calorie row\r\n50 toes-to-bars\r\n40 wall-ball shots\r\n30 cleans\r\n20 muscle-ups\r\n\r\nCompleted: 205 reps', '2023-03-29 12:31:53'),
+(8, 10, '4 rounds for time of:\r\nRun 400 meters\r\n50 squats', '2023-03-02 18:55:00'),
+(9, 3, '4 rounds for time of:\r\nRun 400 meters\r\n50 squats', '2023-03-02 18:55:00'),
+(10, 1, 'Complete as many rounds as possible in 7 minutes of:\r\n10 sumo deadlift high pulls\r\n10 push presses', '2023-03-02 18:55:00');
 
 -- --------------------------------------------------------
 
@@ -139,19 +187,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `likes`
 --
 ALTER TABLE `likes`
-  MODIFY `like_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `like_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `log_posts`
 --
 ALTER TABLE `log_posts`
-  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `user`
