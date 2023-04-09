f9d56e7e6773fbc1aba9ac8eed3ecfb27cb9accd
Aidan Trimmer <trimmeag@dukes.jmu.edu>
2023-04-08 22:49:26 -0400

    this makes connection successful for database!!!!

--- db_connection.php
+++ db_connection.php
@@ -1,16 +1,25 @@
 
-
 <?php
 $servername = "db5012579411.hosting-data.io";
 $username = "dbu486281";
 $password = "L@XXmwKtbZax6Gqo";
-
-try {
-  $conn = new PDO("mysql:host=$servername;dbname= dbs10574449", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+$database = "dbs10574449";
+// create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
 }
+echo "Connected successfully";
+
+
+
+// try {
+//   $conn = new PDO("mysql:host=$servername;dbname=dbs10574449", $username, $password);
+//   // set the PDO error mode to exception
+//   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//   echo "Connected successfully";
+// } catch(PDOException $e) {
+//   echo "Connection failed: " . $e->getMessage();
+// }
 ?>
