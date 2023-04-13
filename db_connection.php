130b608481affd343bfed1affd0ce85e3a8d30cd
Aidan Trimmer <trimmeag@dukes.jmu.edu>
2023-04-13 13:29:40 -0400

    THIS DATABASE CONNECTION IS WORKING

--- db_connection.php
+++ db_connection.php
@@ -1,16 +1,16 @@
+
 <?php
   $host_name = 'db5012579411.hosting-data.io';
   $database = 'dbs10574449';
   $user_name = 'dbu486281';
   $password = 'L@XXmwKtbZax6Gqo';
+  $conn = null;
 
-  $link = new mysqli($host_name, $user_name, $password, $database);
-
-  if ($link->connect_error) {
-    die('<p>Failed to connect to MySQL: '. $link->connect_error .'</p>');
-  } else {
-    echo '<p>Connection to MySQL server successfully established.</p>';
+  try {
+    $conn = new PDO("mysql:host=$host_name; dbname=$database;", $user_name, $password);
+  } catch (PDOException $e) {
+    echo "Error!:" . $e->getMessage() . "<br/>";
+    die();
   }
-
   //$conn = new PDO("mysql:host=localhost;dbname=cs347_project", "root", "");
 ?>
